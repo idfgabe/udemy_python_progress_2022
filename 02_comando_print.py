40f24703de26dd8dcaaed4797c7059c0e62afd52
Gabriel Rolim <93165708+idfgabe@users.noreply.github.com>
2022-03-30 01:52:18 -0300

    update-print

--- 02_comando_print.py
+++ 02_comando_print.py
@@ -1,11 +1,11 @@
-#print('Ola Mundo')
-#funcao///argumento 
+#print('Olá Mundo')
+#função///argumento 
 #print('que', 'coisa', 'legal') 
-#c;s-argumentos tem que separar por virgula
-# print('Gabriel', 'Mariana', sep=' <3 ', end='')
+#argumentos têm que ser separados por vírgula
+print('Gabriel', 'Mariana', sep=' <3 ', end='')
 '''
 123.456.789-10
-Exercicio: 
+Exercício: 
 print CPF usando SEP e END
 '''
 print('123', '456', '789', sep='.', end='-')
