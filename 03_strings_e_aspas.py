55a399fe3fbc3780677053d2165271222e9347cb
Gabriel Rolim <93165708+idfgabe@users.noreply.github.com>
2022-03-30 01:52:55 -0300

    update-strings

--- 03_strings_e_aspas.py
+++ 03_strings_e_aspas.py
@@ -2,8 +2,8 @@
 string - str
 podem ser escritas com aspas simples ou duplas
 """
-print('Esse texto e uma string')
+print('Esse texto é uma string')
 print('Esse texto possui "aspas duplas"')
 print("Esse texto possui 'aspas simples'")
-print('Outra forma de representar \'aspas\' e usando a barra invertida')
+print('Outra forma de representar \'aspas\' é usando a barra invertida')
 print(r'Aqui estou usando o "r" para raw. exemplo: print(ola mundo)')
