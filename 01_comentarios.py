8757b4fda1661b867ce00d609f65eca31430566a
Gabriel Rolim <93165708+idfgabe@users.noreply.github.com>
2022-03-30 01:51:16 -0300

    update-comentarios

--- 01_comentarios.py
+++ 01_comentarios.py
@@ -1,7 +1,7 @@
 # Isso é um comentário na linha 1
-# print('Linha 1') ESSE ESTA COMENTADO
+# print('Linha 1') ESSE ESTÁ COMENTADO
 print('Linha 2')  # Comentario na linha 2
-''' bloco de comentarios
+''' bloco de comentários
 print('Linha 3')
 print('Linha 4')
 '''
