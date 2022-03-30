9ed090060ec5707e40ebbff52474aa698b91dda5
Gabriel Rolim <93165708+idfgabe@users.noreply.github.com>
2022-03-30 01:56:28 -0300

    update-imput

--- 09_input.py
+++ 09_input.py
@@ -3,7 +3,12 @@ Entrada de Dados
 usa-se o input
 """
 nome = input('Qual o seu nome? ')
-idade = input('Qual a sua idade? ')
-ano_nascimento = 2022-int(idade)
+idade = int(input('Qual a sua idade? '))
+ano_nascimento = 2022-idade
 print(f'O usuário {nome} tem {idade} anos. ')
 print(f'{nome} nasceu em {ano_nascimento}')
+
+#Calculadora de soma simples
+var_numero1 = int(input('Digite um número a ser somado: '))
+var_numero2 = int(input('Digite outro número: '))
+var_resultado_soma = print(var_numero1 + var_numero2)
